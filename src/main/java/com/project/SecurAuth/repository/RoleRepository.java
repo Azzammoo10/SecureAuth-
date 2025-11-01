2471636d7d9202f35c3a28230f1b19c758559dff
riyad4589 <riyadmaj10@gmail.com>
2025-11-01 18:29:19 +0100

    Initial Commit

--- src/main/java/com/project/SecurAuth/repository/RoleRepository.java
+++ src/main/java/com/project/SecurAuth/repository/RoleRepository.java
@@ -1,4 +1,7 @@
 package com.project.SecurAuth.repository;
 
-public interface RoleRepository {
+import com.project.SecurAuth.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
 }
