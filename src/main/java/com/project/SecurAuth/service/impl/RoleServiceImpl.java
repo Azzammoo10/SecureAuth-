4d3d43f8683c088f1ef6b9c2d556d18973a8aa89
AZZAM <azzam.moo10@gmail.com>
2025-11-03 15:55:23 +0100

    add roleservice

--- src/main/java/com/project/SecurAuth/service/impl/RoleServiceImpl.java
+++ src/main/java/com/project/SecurAuth/service/impl/RoleServiceImpl.java
@@ -1,4 +1,39 @@
 package com.project.SecurAuth.service.impl;
 
-public class RoleServiceImpl {
+import com.project.SecurAuth.dto.RoleRequest;
+import com.project.SecurAuth.entity.Role;
+import com.project.SecurAuth.service.interfaces.RoleService;
+
+import java.util.List;
+
+public class RoleServiceImpl implements RoleService {
+    @Override
+    public Role createRole(RoleRequest request) {
+        return null;
+    }
+
+    @Override
+    public void deleteRole(Long id) {
+
+    }
+
+    @Override
+    public Role getRoleByName(String name) {
+        return null;
+    }
+
+    @Override
+    public List<Role> getAllRoles() {
+        return List.of();
+    }
+
+    @Override
+    public void assignRoleToUser(Long userId, Long roleId) {
+
+    }
+
+    @Override
+    public void removeRoleFromUser(Long userId, Long roleId) {
+
+    }
 }
